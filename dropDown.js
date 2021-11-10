39c0373ac74155ebb29c3fa6d9d62e790f6b7761
Banntex <danielrk12345@hotmail.com>
2021-11-10 14:09:32 +0100

    Fixed but not really

---
+++ dropDown.js
@@ -0,0 +1,25 @@
+// Closes dropdown onclick outside the box
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// This toggles the dropdown menu on btn press
+function toggleDropdown(dropdown_id) {
+  document.getElementById(dropdown_id).classList.toggle("show");
+}
+
+
+// This can be used on article as quicklinks to the actual article
+function onArticlePress(elem){
+    var title = elem.querySelector("#article_title").innerHTML;
+    location.href = title.trim() + ".html";
+}
